a282332bbdf43bacc18d2598728a337a2a8798b1
Ming <82824731+MingTheNoob@users.noreply.github.com>
2022-12-29 23:59:44 -0500

    Update main.js

--- utilities/web-tools/gddllg/main.js
+++ utilities/web-tools/gddllg/main.js
@@ -1,3 +1,11 @@
 new ClipboardJS('.btn', {
 	container: document.getElementById('copylinkbtn')
 });
+
+clipboard.on('success', function(e) {
+    document.getElementById('copylinkbtn').innerHTML = 'Copied';
+});
+
+clipboard.on('error', function(e) {
+    document.getElementById('copylinkbtn').innerHTML = 'Error';
+});
