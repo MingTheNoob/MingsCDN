0e96809acc4c42ab32591b9d04b54dc2ac5f799f
Ming <82824731+MingTheNoob@users.noreply.github.com>
2022-12-30 00:24:09 -0500

    Update main.js

--- utilities/web-tools/gddllg/main.js
+++ utilities/web-tools/gddllg/main.js
@@ -1,3 +1,23 @@
+var $shareLink = $('#sharelink'),
+    $downloadLink = $('#downloadlink'),
+    $copyButton = $('#copylinkbtn');
+
+$shareLink.on('keyup paste', function () {
+    var link = $shareLink.val(),
+        l = link.replace(/\/file\/d\/(.+)\/(.+)/, "/uc?export=download&id=$1");
+    if (l !== link) {
+        $downloadLink.val(l);
+        $copyButton.removeAttr('disabled');
+    } else {
+        $downloadLink.val('');
+        $copyButton.attr('disabled', 'disabled');
+    }
+});
+
+$downloadLink.on('click', function () {
+    $downloadLink.select();
+});
+
 var clipboard = new ClipboardJS('.btn', {
     container: document.getElementById('copylinkbtn')
 });
